70a7a155531cff297bf6fe51689f00584ea045e6
Cody Bontecou <bontecouc@gmail.com>
2024-12-20 18:36:06 -0600

    basic langswitch component

---
+++ components/LangSwitcher.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { locales, locale, setLocale } = useI18n()
+const localePath = useLocalePath()
+
+const language = computed({
+    get: () => locale.value,
+    set: value => {
+        setLocale(value)
+        navigateTo(localePath('/', value))
+    },
+})
+</script>
+<template>
+    <div>
+        <select v-model="language">
+            <option v-for="item in locales" :key="item.code" :value="item.code">
+                {{ item.name }}
+            </option>
+        </select>
+    </div>
+</template>
