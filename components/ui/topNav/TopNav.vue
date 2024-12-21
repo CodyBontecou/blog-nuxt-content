8d63685dc7cfbafde5e954d5770c4fe36c0cd042
Cody Bontecou <bontecouc@gmail.com>
2024-12-20 18:36:25 -0600

    NuxtLinkLocale on topnav

--- components/ui/topNav/TopNav.vue
+++ components/ui/topNav/TopNav.vue
@@ -1,11 +1,18 @@
 <template>
     <header class="flex justify-between items-center mb-16">
         <h1 class="">
-            <NuxtLink to="/" class="hover:opacity-75"> Cody Bontecou </NuxtLink>
+            <NuxtLinkLocale to="/" class="hover:opacity-75">
+                Cody Bontecou
+            </NuxtLinkLocale>
         </h1>
         <nav class="flex gap-6 text-gray-600">
-            <NuxtLink to="/"> {{ $t('writing.writing') }} </NuxtLink>
-            <NuxtLink to="/about"> {{ $t('landing.learnMore') }} </NuxtLink>
+            <NuxtLinkLocale to="/">
+                {{ $t('writing.writing') }}
+            </NuxtLinkLocale>
+            <NuxtLinkLocale to="/about">
+                {{ $t('landing.learnMore') }}
+            </NuxtLinkLocale>
+            <LangSwitcher />
         </nav>
     </header>
 </template>
